fdb63dfefdbb86afc19856c389d418f8a2c559a0
k2wl <k2wl@yahoo.com>
2013-06-22 09:59:07 +0530

    first commit 3.0.82

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Jun 21 22:31:01 IST 2013"
+#define LINUX_COMPILE_BY "kar"
+#define LINUX_COMPILE_HOST "kar-ThinkPad-T400"
+#define LINUX_COMPILER "gcc version 4.8.2 20130603 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.06) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.82tom_loves_jerry"
