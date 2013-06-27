74167207a86c5b54fd5cc352fbaf8f508003b3bd
k2wl <k2wl@yahoo.com>
2013-06-27 18:18:20 +0530

    new version

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -1,8 +1,8 @@
 #include "vchiq_build_info.h"
 #include <linux/broadcom/vc_debug_sym.h>
 
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "kar-ThinkPad-T400" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "e67b9d6d42a4cc1331c34dfbc704d0092dc2353b (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "Android-ThinkPad-T400" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "858e57de47047b91f5e35e2024c27e9a0dbb6945 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
