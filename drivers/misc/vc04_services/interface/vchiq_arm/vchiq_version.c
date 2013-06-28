06f08ecf565dcef4917fb9b1dba3f187e20628ba
k2wl <k2wl@yahoo.com>
2013-06-28 08:00:55 +0530

    some changes

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "Android-ThinkPad-T400" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "5b191ed293a5f089b382d60b4ba25c4837f07416 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "c423c313c94546b3021637b1198c6c32dd750c71 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
